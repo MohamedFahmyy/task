60db0041f000d1577a828c63f11cd302b6d27aca
Mohamed Fahmy <M.Fahmy9800@gmail.com>
2026-08-13 14:47:23 +0300

    style: wrap children in app-layout to isolate from body extensions

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,11 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`} data-theme="dark">
-      <body>{children}</body>
+      <body>
+        <div id="app-layout">
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
